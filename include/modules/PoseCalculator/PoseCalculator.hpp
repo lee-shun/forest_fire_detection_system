2581050b031d1771c9255eb4273b58193093ef34
lee-shun <lee970802@163.com>
2022-03-17 12:02:42 -0400

    u

---
+++ include/modules/PoseCalculator/PoseCalculator.hpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+*   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
+*
+*   @Filename: PoseCalculator.hpp
+*
+*   @Author: Shun Li
+*
+*   @Email: 2015097272@qq.com
+*
+*   @Date: 2022-03-17
+*
+*   @Description: 
+*
+*******************************************************************************/
+
+
+#ifndef INCLUDE_MODULES_POSECALCULATOR_POSECALCULATOR_HPP_
+#define INCLUDE_MODULES_POSECALCULATOR_POSECALCULATOR_HPP_
+
+
+#include ""
+
+namespace FFDS {
+namespace MODULES {
+class PoseCalculator{};
+}
+}
+
+
+#endif  // INCLUDE_MODULES_POSECALCULATOR_POSECALCULATOR_HPP_
