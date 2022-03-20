f6000d0d6182c7e9ac752506b6f1fe6178fe1bb2
lee-shun <lee970802@163.com>
2022-03-20 11:30:41 -0400

    update the file stuff

--- include/tools/SystemLib.hpp
+++ include/tools/SystemLib.hpp
@@ -21,8 +21,15 @@
 #include <sys/time.h>
 #include <tools/PrintControl/PrintCtrlMacro.h>
 #include <yaml-cpp/yaml.h>
+#include <dirent.h>
+#include <sys/io.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include <cstdint>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -31,7 +38,7 @@ namespace FFDS {
 namespace TOOLS {
 
 /* return as second */
-static float getRosTimeInterval(const ros::Time& begin) {
+inline float getRosTimeInterval(const ros::Time& begin) {
   ros::Time time_now = ros::Time::now();
   float currTimeSec = time_now.sec - begin.sec;
   float currTimenSec = time_now.nsec / 1e9 - begin.nsec / 1e9;
@@ -39,21 +46,17 @@ static float getRosTimeInterval(const ros::Time& begin) {
 }
 
 /* return as ms */
-static int32_t getSysTime() {
+inline int32_t getSysTime() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
 /* return as ms */
-static int32_t getTimeInterval(const int32_t begin_time) {
+inline int32_t getTimeInterval(const int32_t begin_time) {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   return (tv.tv_sec * 1000 + tv.tv_usec / 1000 - begin_time);
-}
-
-static std::string addStr(const std::string a, const std::string b) {
-  return a + b;
 }
 
 template <typename T>
@@ -69,6 +72,59 @@ T getParam(const YAML::Node& node, const std::string& paramName,
                     << paramName << ", assigning default: " << v);
   }
   return v;
+}
+
+/* create a dir (must be single level) if not exists */
+inline bool creatDir(const std::string dir) {
+  if (access(dir.c_str(), 0) == -1) {
+    int flag = mkdir(dir.c_str(), S_IRWXU);
+    if (flag == 0) {
+      PRINT_INFO("dir: %s created successfully!", dir.c_str());
+    } else {
+      PRINT_ERROR("dir: %s created failed!", dir.c_str());
+      return false;
+    }
+  } else {
+    PRINT_WARN("dir: %s already exists!", dir.c_str());
+  }
+  return true;
+}
+
+/* remove single file */
+inline bool removeFile(const std::string filename) {
+  if (access(filename.c_str(), 0) == -1) {
+    PRINT_WARN("filename: %s does not exit!", filename.c_str());
+    return false;
+  }
+
+  if (0 == remove(filename.c_str())) {
+    PRINT_INFO("remove file %s successfully!", filename.c_str());
+    return true;
+  } else {
+    PRINT_ERROR("remove file %s failed!", filename.c_str());
+    return false;
+  }
+}
+
+// https://blog.csdn.net/cheyo/article/details/6595955
+inline bool shellStatus(pid_t& status) {
+  if ((-1 != status) && (WIFEXITED(status)) && (0 == WEXITSTATUS(status))) {
+    return true;
+  }
+
+  return false;
+}
+
+inline bool shellRm(const std::string path) {
+  PRINT_WARN("rm -rf %s", path.c_str());
+  pid_t status = std::system(("rm -rf " + path).c_str());
+  return shellStatus(status);
+}
+
+inline bool shellMkdir(const std::string path) {
+  PRINT_WARN("mkdir -p %s", path.c_str());
+  pid_t status = std::system(("mkdir -p " + path).c_str());
+  return shellStatus(status);
 }
 
 }  // namespace TOOLS
---
+++ src/tools/SaveM300FrontStereoImu.cpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+*   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
+*
+*   @Filename: SaveM300FrontStereoImu.cpp
+*
+*   @Author: Shun Li
+*
+*   @Email: 2015097272@qq.com
+*
+*   @Date: 2022-03-20
+*
+*   @Description: 
+*
+*******************************************************************************/
+
+
+#include "modules/StereoCameraOperator/StereoCamOperator.hpp"
+#include "tools/SystemLib.hpp"
+int main(int argc, char** argv) {
+return 0;
+}
