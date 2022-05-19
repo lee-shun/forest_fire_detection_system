ea52e400c752d532bc9c300e6a4933e308668127
lee-shun <lee970802@163.com>
2022-05-18 21:51:41 -0400

    update

---
+++ include/modules/H20TIMUPoseGrabber/H20TIMUPoseGrabber.hpp
@@ -0,0 +1,113 @@
+/*******************************************************************************
+ *   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
+ *
+ *   @Filename: H20TIMUPoseGrabber.hpp
+ *
+ *   @Author: Shun Li
+ *
+ *   @Email: 2015097272@qq.com
+ *
+ *   @Date: 2022-05-18
+ *
+ *   @Description:
+ *
+ *******************************************************************************/
+
+#ifndef INCLUDE_MODULES_H20TIMUPOSEGRABBER_H20TIMUPOSEGRABBER_HPP_
+#define INCLUDE_MODULES_H20TIMUPOSEGRABBER_H20TIMUPOSEGRABBER_HPP_
+
+#include <ros/ros.h>
+
+// stereo camera
+#include <dji_osdk_ros/StereoVGASubscription.h>
+#include <sensor_msgs/Image.h>
+
+// local and global pose
+#include <dji_osdk_ros/SetLocalPosRef.h>
+#include <geometry_msgs/PointStamped.h>
+#include <sensor_msgs/NavSatFix.h>
+
+// attitude
+#include <geometry_msgs/QuaternionStamped.h>
+
+// asyn sensor data
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <string>
+
+// asyn data struct
+#include <common/DepthImgWithPoseAtt.hpp>
+
+// handle main camera images
+#include <dji_ack.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+namespace FFDS {
+namespace MODULES {
+class H20TIMUPoseGrabber {
+ public:
+  enum MessageFilterStatus { EMPTY, NORMAL };
+
+  H20TIMUPoseGrabber();
+
+  MessageFilterStatus UpdateOnce() {
+    ros::spinOnce();
+    if (rgb_img_.empty() || ir_img_.empty()) {
+      message_filter_status_ = MessageFilterStatus::EMPTY;
+    } else {
+      message_filter_status_ = MessageFilterStatus::NORMAL;
+    }
+    return message_filter_status_;
+  }
+
+  cv::Mat GetRGBImageOnce() const { return rgb_img_.clone(); }
+  cv::Mat GetIRImageOnce() const { return ir_img_.clone(); }
+
+  sensor_msgs::NavSatFix GetGPSPoseOnce() { return gps_pos_; }
+  geometry_msgs::PointStamped GetLocalPosOnce() { return local_pos_; }
+  geometry_msgs::QuaternionStamped GetAttOnce() { return att_; }
+
+ private:
+  ros::NodeHandle nh_;
+
+  cv_bridge::CvImagePtr rgb_cvimg_ptr_;
+  cv_bridge::CvImagePtr ir_cvimg_ptr_;
+  cv::Mat rgb_img_;
+  cv::Mat ir_img_;
+
+  sensor_msgs::NavSatFix gps_pos_;
+  geometry_msgs::PointStamped local_pos_;
+  geometry_msgs::QuaternionStamped att_;
+
+  MessageFilterStatus message_filter_status_{MessageFilterStatus::EMPTY};
+
+  /**
+   * msg
+   * */
+  message_filters::Subscriber<sensor_msgs::NavSatFix> gps_sub_;
+  message_filters::Subscriber<geometry_msgs::PointStamped> local_pos_sub_;
+  message_filters::Subscriber<geometry_msgs::QuaternionStamped> attitude_sub_;
+  message_filters::Subscriber<sensor_msgs::Image> img_rgb_sub_;
+  message_filters::Subscriber<sensor_msgs::Image> img_ir_sub_;
+
+  typedef message_filters::sync_policies::ApproximateTime<
+      sensor_msgs::NavSatFix, geometry_msgs::PointStamped,
+      geometry_msgs::QuaternionStamped, sensor_msgs::Image, sensor_msgs::Image>
+      PoseAttStereoSyncPloicy;
+
+  // 1. gps, 2. local pose, 3. rotation, 4. rgb image, 5. ir image
+  message_filters::Synchronizer<PoseAttStereoSyncPloicy>* topic_synchronizer_;
+
+  void SyncCallback(const sensor_msgs::NavSatFixConstPtr& gps_msg,
+                    const geometry_msgs::PointStampedConstPtr& local_pos_msg,
+                    const geometry_msgs::QuaternionStampedConstPtr& att_msg,
+                    const sensor_msgs::ImageConstPtr& rgb_img_msg,
+                    const sensor_msgs::ImageConstPtr& ir_img_msg);
+};
+}  // namespace MODULES
+}  // namespace FFDS
+
+#endif  // INCLUDE_MODULES_H20TIMUPOSEGRABBER_H20TIMUPOSEGRABBER_HPP_
---
+++ src/modules/H20TIMUPoseGrabber/H20TIMUPoseGrabber.cpp
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ *   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
+ *
+ *   @Filename: H20TIMUPoseGrabber.cpp
+ *
+ *   @Author: Shun Li
+ *
+ *   @Email: 2015097272@qq.com
+ *
+ *   @Date: 2022-05-18
+ *
+ *   @Description:
+ *
+ *******************************************************************************/
+
+#include "modules/H20TIMUPoseGrabber/H20TIMUPoseGrabber.hpp"
+#include "tools/PrintControl/PrintCtrlMacro.h"
+
+FFDS::MODULES::H20TIMUPoseGrabber::H20TIMUPoseGrabber() {
+  /**
+   * Step: use filter to sync the sensors data
+   * */
+  gps_sub_.subscribe(nh_, "dji_osdk_ros/gps_position", 1);
+  local_pos_sub_.subscribe(nh_, "dji_osdk_ros/local_position", 1);
+  attitude_sub_.subscribe(nh_, "dji_osdk_ros/attitude", 1);
+  img_rgb_sub_.subscribe(
+      nh_, "forest_fire_detection_system/main_camera_rgb_image", 1);
+  img_ir_sub_.subscribe(nh_,
+                        "forest_fire_detection_system/main_camera_ir_image", 1);
+
+  // synchronizer
+  topic_synchronizer_ =
+      new message_filters::Synchronizer<PoseAttStereoSyncPloicy>(
+          PoseAttStereoSyncPloicy(10), gps_sub_, local_pos_sub_, attitude_sub_,
+          img_rgb_sub_, img_ir_sub_);
+
+  topic_synchronizer_->registerCallback(
+      boost::bind(&H20TIMUPoseGrabber::SyncCallback, this, _1, _2, _3, _4, _5));
+
+  ros::Duration(1.0).sleep();
+  PRINT_INFO("Create H20TIMUPoseGrabber done!");
+}
+
+void FFDS::MODULES::H20TIMUPoseGrabber::SyncCallback(
+    const sensor_msgs::NavSatFixConstPtr& gps_msg,
+    const geometry_msgs::PointStampedConstPtr& local_pos_msg,
+    const geometry_msgs::QuaternionStampedConstPtr& att_msg,
+    const sensor_msgs::ImageConstPtr& rgb_img_msg,
+    const sensor_msgs::ImageConstPtr& ir_img_msg) {
+  att_ = *att_msg;
+  gps_pos_ = *gps_msg;
+  local_pos_ = *local_pos_msg;
+
+  rgb_cvimg_ptr_ =
+      cv_bridge::toCvCopy(rgb_img_msg, sensor_msgs::image_encodings::BGR8);
+  rgb_img_ = rgb_cvimg_ptr_->image;
+
+  ir_cvimg_ptr_ =
+      cv_bridge::toCvCopy(ir_img_msg, sensor_msgs::image_encodings::BGR8);
+  ir_img_ = ir_cvimg_ptr_->image;
+}
